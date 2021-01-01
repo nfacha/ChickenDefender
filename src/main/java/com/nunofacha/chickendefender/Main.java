0f392545d22231cdd42163920f3cf5630deaa413
Nuno Facha <nunofacha@gmail.com>
2021-01-01 13:10:15 -0100

    WIP

---
+++ src/main/java/com/nunofacha/chickendefender/Main.java
@@ -0,0 +1,26 @@
+package com.nunofacha.chickendefender;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class Main extends JavaPlugin {
+    public static Plugin plugin;
+    public static Logger logger;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        plugin = this;
+        logger = getLogger();
+        logger.info("Plugin is stating");
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
