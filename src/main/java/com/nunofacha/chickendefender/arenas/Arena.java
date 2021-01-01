4c653ed610c765af3195422af0ab6297e91c0ae5
Nuno Facha <nunofacha@gmail.com>
2021-01-01 16:42:29 -0100

    Ability to check if a location is in an arena

--- src/main/java/com/nunofacha/chickendefender/arenas/Arena.java
+++ src/main/java/com/nunofacha/chickendefender/arenas/Arena.java
@@ -3,6 +3,7 @@ package com.nunofacha.chickendefender.arenas;
 import com.nunofacha.chickendefender.Main;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.util.Vector;
 
 @SuppressWarnings({"FieldMayBeFinal", "FieldCanBeLocal", "ConstantConditions"})
 public class Arena {
@@ -16,6 +17,10 @@ public class Arena {
     private Location attackingSpawn;
     private Location defendingSpawn;
     private Location chickenSpawn;
+    private Location corner1;
+    private Location corner2;
+    private Vector minVector;
+    private Vector maxVector;
 
     public Arena(int arenaId) {
         this.arenaId = arenaId;
@@ -27,18 +32,42 @@ public class Arena {
         this.maxPlayers = Main.plugin.getConfig().getInt(this.configPath + ".players.max");
         this.world = Main.plugin.getServer().getWorld(Main.plugin.getConfig().getString(this.configPath + ".locations.world"));
         this.attackingSpawn = new Location(this.world,
-                Main.plugin.getConfig().getInt(this.configPath + ".locations.attacking.x"),
-                Main.plugin.getConfig().getInt(this.configPath + ".locations.attacking.y"),
-                Main.plugin.getConfig().getInt(this.configPath + ".locations.attacking.z"));
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.spawn.attacking.x"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.spawn.attacking.y"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.spawn.attacking.z"));
         this.defendingSpawn = new Location(this.world,
-                Main.plugin.getConfig().getInt(this.configPath + ".locations.defending.x"),
-                Main.plugin.getConfig().getInt(this.configPath + ".locations.defending.y"),
-                Main.plugin.getConfig().getInt(this.configPath + ".locations.defending.z"));
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.spawn.defending.x"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.spawn.defending.y"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.spawn.defending.z"));
         this.chickenSpawn = new Location(this.world,
-                Main.plugin.getConfig().getInt(this.configPath + ".locations.chicken.x"),
-                Main.plugin.getConfig().getInt(this.configPath + ".locations.chicken.y"),
-                Main.plugin.getConfig().getInt(this.configPath + ".locations.chicken.z"));
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.spawn.chicken.x"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.spawn.chicken.y"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.spawn.chicken.z"));
+        this.corner1 = new Location(this.world,
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.corner1.x"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.corner1.y"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.corner1.z"));
 
+        int xPos1 = Math.min(Main.plugin.getConfig().getInt(this.configPath + ".locations.corner1.x"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.corner2.x"));
+        int yPos1 = Math.min(Main.plugin.getConfig().getInt(this.configPath + ".locations.corner1.y"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.corner2.y"));
+        int zPos1 = Math.min(Main.plugin.getConfig().getInt(this.configPath + ".locations.corner1.z"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.corner2.z"));
+
+        int xPos2 = Math.max(Main.plugin.getConfig().getInt(this.configPath + ".locations.corner1.x"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.corner2.x"));
+        int yPos2 = Math.max(Main.plugin.getConfig().getInt(this.configPath + ".locations.corner1.y"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.corner2.y"));
+        int zPos2 = Math.max(Main.plugin.getConfig().getInt(this.configPath + ".locations.corner1.z"),
+                Main.plugin.getConfig().getInt(this.configPath + ".locations.corner2.z"));
+
+        minVector = new Vector(xPos1, yPos1, zPos1);
+        maxVector = new Vector(xPos2, yPos2, zPos2);
         Main.logger.info("Arena " + this.name + " was loaded with ID " + arenaId);
+    }
+
+    public boolean containsLocation(Location location) {
+        return location.toVector().isInAABB(minVector, maxVector);
     }
 }
