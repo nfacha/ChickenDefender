7a1e2e27321717be61f94eef2d327cdb63f94c7a
Nuno Facha <nunofacha@gmail.com>
2021-01-01 17:21:39 -0100

    Countdown and game start

--- src/main/java/com/nunofacha/chickendefender/arenas/Arena.java
+++ src/main/java/com/nunofacha/chickendefender/arenas/Arena.java
@@ -1,6 +1,7 @@
 package com.nunofacha.chickendefender.arenas;
 
 import com.nunofacha.chickendefender.Main;
+import com.nunofacha.chickendefender.arenas.game.Countdown;
 import com.nunofacha.chickendefender.arenas.game.GameState;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -28,6 +29,7 @@ public class Arena {
     private Vector maxVector;
     private ArrayList<UUID> players;
     private GameState state;
+    private Countdown countdown;
 
     public Arena(int arenaId) {
         this.arenaId = arenaId;
@@ -72,6 +74,7 @@ public class Arena {
         minVector = new Vector(xPos1, yPos1, zPos1);
         maxVector = new Vector(xPos2, yPos2, zPos2);
         state = GameState.RECRUITING;
+        countdown = new Countdown(this, 30);
         Main.logger.info("Arena " + this.name + " was loaded with ID " + arenaId);
     }
 
@@ -95,11 +98,141 @@ public class Arena {
         players.add(p.getUniqueId());
         p.teleport(Main.arenaManager.getLobbyLocation());
         p.sendMessage("You have been teleported to arena lobby");
+        if (players.size() >= getMinPlayers()) {
+            countdown.begin();
+        }
     }
 
     public void removePlayer(Player p) {
         players.remove(p.getUniqueId());
         p.teleport(Main.arenaManager.getLobbyLocation());
         p.sendMessage("You left the arena");
+    }
+
+    public GameState getState() {
+        return state;
+    }
+
+    public void setState(GameState state) {
+        this.state = state;
+    }
+
+    public void start() {
+
+    }
+
+    public void sendMessageToAll(String msg) {
+        for (UUID uuid : players) {
+            Player p = Main.plugin.getServer().getPlayer(uuid);
+            p.sendMessage(msg);
+        }
+    }
+
+    public String getConfigPath() {
+        return configPath;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isTeamSelection() {
+        return teamSelection;
+    }
+
+    public void setTeamSelection(boolean teamSelection) {
+        this.teamSelection = teamSelection;
+    }
+
+    public int getMinPlayers() {
+        return minPlayers;
+    }
+
+    public void setMinPlayers(int minPlayers) {
+        this.minPlayers = minPlayers;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public void setMaxPlayers(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public World getWorld() {
+        return world;
+    }
+
+    public void setWorld(World world) {
+        this.world = world;
+    }
+
+    public Location getAttackingSpawn() {
+        return attackingSpawn;
+    }
+
+    public void setAttackingSpawn(Location attackingSpawn) {
+        this.attackingSpawn = attackingSpawn;
+    }
+
+    public Location getDefendingSpawn() {
+        return defendingSpawn;
+    }
+
+    public void setDefendingSpawn(Location defendingSpawn) {
+        this.defendingSpawn = defendingSpawn;
+    }
+
+    public Location getChickenSpawn() {
+        return chickenSpawn;
+    }
+
+    public void setChickenSpawn(Location chickenSpawn) {
+        this.chickenSpawn = chickenSpawn;
+    }
+
+    public Location getCorner1() {
+        return corner1;
+    }
+
+    public void setCorner1(Location corner1) {
+        this.corner1 = corner1;
+    }
+
+    public Location getCorner2() {
+        return corner2;
+    }
+
+    public void setCorner2(Location corner2) {
+        this.corner2 = corner2;
+    }
+
+    public Vector getMinVector() {
+        return minVector;
+    }
+
+    public void setMinVector(Vector minVector) {
+        this.minVector = minVector;
+    }
+
+    public Vector getMaxVector() {
+        return maxVector;
+    }
+
+    public void setMaxVector(Vector maxVector) {
+        this.maxVector = maxVector;
+    }
+
+    public void setPlayers(ArrayList<UUID> players) {
+        this.players = players;
+    }
+
+    public Countdown getCountdown() {
+        return countdown;
+    }
+
+    public void setCountdown(Countdown countdown) {
+        this.countdown = countdown;
     }
 }
--- src/main/java/com/nunofacha/chickendefender/arenas/game/Countdown.java
+++ src/main/java/com/nunofacha/chickendefender/arenas/game/Countdown.java
@@ -1,4 +1,38 @@
 package com.nunofacha.chickendefender.arenas.game;
 
-public class Countdown {
+import com.nunofacha.chickendefender.Main;
+import com.nunofacha.chickendefender.arenas.Arena;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Countdown extends BukkitRunnable {
+    private Arena arena;
+    private int seconds;
+
+    public Countdown(Arena arena, int seconds) {
+        this.arena = arena;
+        this.seconds = seconds;
+    }
+
+    public void begin() {
+        this.arena.setState(GameState.COUNTDOWN);
+        this.runTaskTimer(Main.plugin, 0, 20);
+    }
+
+    @Override
+    public void run() {
+        if (this.seconds == 0) {
+            cancel();
+            arena.start();
+        }
+        if (seconds % 30 == 0 || seconds <= 10) {
+            arena.sendMessageToAll("The game will start in " + seconds + " seconds!");
+        }
+        if (arena.getPlayers().size() < arena.getMinPlayers()) {
+            cancel();
+            arena.setState(GameState.RECRUITING);
+            Main.logger.info("Start canceled due to insufficient players for arena " + arena.getName());
+            arena.sendMessageToAll("Too few player, countdown canceled while we wait for more players");
+        }
+        seconds--;
+    }
 }
