24fa3f519054cc953c242d1fe60ce0e2d462d4dd
Nuno Facha <nunofacha@gmail.com>
2021-01-25 16:03:36 -0100

    Fix sign detection

--- src/main/java/com/nunofacha/chickendefender/listeners/SignListener.java
+++ src/main/java/com/nunofacha/chickendefender/listeners/SignListener.java
@@ -16,21 +16,21 @@ public class SignListener implements Listener {
     public void onSignClick(PlayerInteractEvent e){
         Block block = e.getClickedBlock();
         if(block.getType().toString().contains("WALL_SIGN")){
+            Main.logger.info("Clicked sign");
             Sign sign = (Sign) block.getState();
             if(sign.getLine(0).contains("CHICKEN-DEF")){
+                Main.logger.info("Clicked chicken-def");
                 Arena arena = Main.arenaManager.getArena(Integer.parseInt(sign.getLine(1)));
-                if(arena.getSignLocation() == block.getLocation()){
-                    Player p = e.getPlayer();
-                    if (Main.arenaManager.isPlaying(p)) {
-                        p.sendMessage("You are already on an arena");
-                        return;
-                    }
-                    if (!arena.getState().equals(GameState.RECRUITING) && (!arena.getState().equals(GameState.COUNTDOWN) && arena.getPlayers().size() <= arena.getMaxPlayers())) {
-                        p.sendMessage("Arena not ready");
-                        return;
-                    }
-                    arena.addPlayer(p);
+                Player p = e.getPlayer();
+                if (Main.arenaManager.isPlaying(p)) {
+                    p.sendMessage("You are already on an arena");
+                    return;
                 }
+                if (!arena.getState().equals(GameState.RECRUITING) && (!arena.getState().equals(GameState.COUNTDOWN) && arena.getPlayers().size() <= arena.getMaxPlayers())) {
+                    p.sendMessage("Arena not ready");
+                    return;
+                }
+                arena.addPlayer(p);
             }
         }
     }
