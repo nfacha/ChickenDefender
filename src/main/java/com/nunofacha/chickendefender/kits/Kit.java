454fec986d3cbf6be1956a46232be3968293a3c0
Nuno Facha <nunofacha@gmail.com>
2021-01-26 13:47:39 -0100

    WIP: Loading kit items

--- src/main/java/com/nunofacha/chickendefender/kits/Kit.java
+++ src/main/java/com/nunofacha/chickendefender/kits/Kit.java
@@ -1,13 +1,24 @@
 package com.nunofacha.chickendefender.kits;
 
 import com.nunofacha.chickendefender.Main;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class Kit {
-    private String configKey;
     private ItemStack[] items;
-    public Kit(String name) {
-        this.configKey = "kits."+configKey;
-        Main.logger.info("Loading kit "+name);
+    private String name;
+
+    public Kit(String key) {
+        String configKey = "kits." + key;
+        this.name = Main.plugin.getConfig().getString(configKey + ".name");
+        Main.logger.info("Loading key " + key + " with name " + name);
+        for (String itemKeyId : Main.plugin.getConfig().getConfigurationSection(configKey + ".items").getKeys(false)) {
+//            Main.logger.info("Loading item "+itemKeyId);
+            String itemKey = configKey + ".items." + itemKeyId;
+//            Main.logger.info("Item path "+itemKey);
+            //noinspection ConstantConditions
+            ItemStack itemStack = new ItemStack(Material.getMaterial(Main.plugin.getConfig().getString(itemKey + ".type")));
+//            Main.logger.info("Loaded itemstack of type "+itemStack.getType().toString());
+        }
     }
 }
